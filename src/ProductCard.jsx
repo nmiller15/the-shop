25daa9280cf738881190112a6fa87e671fda01dc
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-04 19:40:26 -0400

    add links to products

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function ProductCard({ product }) {
   ProductCard.propTypes = {
@@ -10,22 +11,24 @@ function ProductCard({ product }) {
 
   return (
     <div className="group mx-auto my-2 w-10/12 cursor-pointer rounded-md bg-gray-200 p-4 md:w-5/12 lg:w-72">
-      <div className="mb-3 flex h-96 w-full place-content-center rounded-md bg-white">
-        <div className="absolute flex h-10 w-16 translate-x-24 translate-y-4 items-center justify-center gap-2 rounded-sm bg-gray-200 p-2 md:translate-x-20 lg:translate-x-[4.5rem]">
-          <p className="font-semibold lg:text-sm">{product.rating.rate}</p>
-          <i className="iconoir-star-solid text-md lg:text-sm"></i>
+      <Link to={`/products/${product.id}`}>
+        <div className="mb-3 flex h-96 w-full place-content-center rounded-md bg-white">
+          <div className="absolute flex h-10 w-16 translate-x-24 translate-y-4 items-center justify-center gap-2 rounded-sm bg-gray-200 p-2 md:translate-x-20 lg:translate-x-[4.5rem]">
+            <p className="font-semibold lg:text-sm">{product.rating.rate}</p>
+            <i className="iconoir-star-solid text-md lg:text-sm"></i>
+          </div>
+          <img src={product.image} className="object-scale-down" />
         </div>
-        <img src={product.image} className="object-scale-down" />
-      </div>
-      <p className="text-2xl font-bold lg:text-lg">{`$${product.price.toFixed(2)}`}</p>
-      <h2
-        className={`truncate text-lg font-semibold lg:text-sm ${quickAddHover ? "" : "group-hover:text-clip group-hover:underline"}`}
-      >
-        {product.title}
-      </h2>
-      <p className="h-12 overflow-clip text-ellipsis text-balance lg:text-xs">
-        {product.description}
-      </p>
+        <p className="text-2xl font-bold lg:text-lg">{`$${product.price.toFixed(2)}`}</p>
+        <h2
+          className={`truncate text-lg font-semibold lg:text-sm ${quickAddHover ? "" : "group-hover:text-clip group-hover:underline"}`}
+        >
+          {product.title}
+        </h2>
+        <p className="h-12 overflow-clip text-ellipsis text-balance lg:text-xs">
+          {product.description}
+        </p>
+      </Link>
       <button
         onMouseOver={() => setQuickAddHover(true)}
         onMouseOut={() => setQuickAddHover(false)}
