c05871bff81eab9909eec797f1e7c1b5de706930
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-09 15:46:46 -0400

    add address_id reference to orders table in schema

---
+++ backend/database/create_tables.sql
@@ -0,0 +1,95 @@
+CREATE TABLE "users" (
+  "id" integer PRIMARY KEY,
+  "username" varchar UNIQUE,
+  "password" varchar,
+  "first_name" varchar,
+  "last_name" varchar,
+  "date_created" datetime,
+  "is_admin" boolean
+);
+
+CREATE TABLE "addresses" (
+  "id" integer PRIMARY KEY,
+  "user_id" integer,
+  "street_one" varchar,
+  "street_two" varchar,
+  "city" varchar,
+  "state" varchar,
+  "zip" integer
+);
+
+CREATE TABLE "products" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "description" text,
+  "category_id" varchar,
+  "rating" integer,
+  "price" money
+);
+
+CREATE TABLE "carts" (
+  "id" index,
+  "username" varchar
+);
+
+CREATE TABLE "orders" (
+  "number" integer PRIMARY KEY,
+  "date_created" datetime,
+  "status" varchar,
+  "address_id" integer,
+  "customer_id" varchar
+);
+
+CREATE TABLE "categories" (
+  "id" varchar PRIMARY KEY,
+  "category_string" varchar
+);
+
+CREATE TABLE "product_images" (
+  "id" varchar PRIMARY KEY,
+  "product_id" integer,
+  "path" varchar UNIQUE
+);
+
+CREATE TABLE "products_carts" (
+  "product_id" varchar,
+  "cart_id" varchar
+);
+
+CREATE TABLE "products_orders" (
+  "product_id" varchar,
+  "order_number" integer
+);
+
+CREATE TABLE "users_addresses" (
+  "user_id" integer,
+  "address_id" integer
+);
+
+COMMENT ON TABLE "users" IS 'Join to carts on username';
+
+COMMENT ON TABLE "orders" IS 'Status can be: "submitted" "shipped" or "fulfilled"';
+
+COMMENT ON TABLE "product_images" IS 'Separate table to allow for muliple images for each product.';
+
+ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+
+ALTER TABLE "carts" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("customer_id") REFERENCES "users" ("username");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("address_id") REFERENCES "addresses" ("id");
+
+ALTER TABLE "product_images" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+ALTER TABLE "products_carts" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+ALTER TABLE "products_carts" ADD FOREIGN KEY ("cart_id") REFERENCES "carts" ("id");
+
+ALTER TABLE "products_orders" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+ALTER TABLE "products_orders" ADD FOREIGN KEY ("order_number") REFERENCES "orders" ("number");
+
+ALTER TABLE "users_addresses" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "users_addresses" ADD FOREIGN KEY ("address_id") REFERENCES "addresses" ("id");
