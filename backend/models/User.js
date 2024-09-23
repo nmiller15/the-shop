d13e26d31099254c2878bf53364bb4c4788e7fd0
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-23 15:43:27 -0400

    refactor constructor, remove methods until known that it is needed

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,13 @@
 class User {
-  id = null;
-  username = "";
-  firstName = "";
-  lastName = "";
-  dateCreated = null;
-  role = "";
-  addresses = [];
-
   constructor(username) {
     this.username = username;
-  }
 
-  getUserData() {
-    fetch(`${API_URI}/${this.username}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((user) => {
-        this.id = user.id;
-        this.firstName = user.firstName;
-        this.lastName = user.lastName;
-        this.role = user.role;
-      })
-      .catch((err) => console.error("Failed to fetch user data:", err));
+    this.id = null;
+    this.firstName = "";
+    this.lastName = "";
+    this.dateCreated = null;
+    this.role = "";
+    this.addresses = [];
   }
 }
 
