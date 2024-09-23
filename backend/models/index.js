cd62939baac9259b2215ed12f9d90dcb9ea1147f
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-23 16:25:02 -0400

    create barrel for model importing

---
+++ backend/models/index.js
@@ -0,0 +1,15 @@
+const User = require("./User");
+const Product = require("./Product");
+const Address = require("./Address");
+const Order = require("./Order");
+const Cart = require("./Cart");
+const ProductImage = require("./ProductImage");
+
+module.exports = {
+  User,
+  Product,
+  Address,
+  Order,
+  Cart,
+  ProductImage,
+};
