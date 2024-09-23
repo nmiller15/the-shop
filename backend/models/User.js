f7f6404cf4096f95b8cb31b7f8600884ea188e09
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-23 15:22:28 -0400

    add User class

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+class User {
+  id;
+  username;
+  firstName;
+  lastName;
+  dateCreated;
+  role;
+  addresses = [];
+
+  constructor(username) {
+    this.username = username;
+  }
+
+  getUserData() {
+    fetch(`${API_URI}/${this.username}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((user) => {
+        this.id = user.id;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.role = user.role;
+      })
+      .catch((err) => console.error("Failed to fetch user data:", err));
+  }
+}
+
+module.exports = User;
