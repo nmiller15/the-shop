28f3396ad079b0b94f4244b693293db9a2b4935e
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-23 15:09:50 -0400

    remove old structure

--- backend/routers/accountsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const accountsRouter = express.Router();
-
-accountsRouter.get('/test', (req, res)=> {
-    res.json('accounts - test ok')
-});
-
-module.exports = accountsRouter;
--- backend/util/dateCreated.js
+++
@@ -1,9 +0,0 @@
-const formatDate = require('date-format');
-
-const dateCreated = (req, res, next) => {
-    const date = new Date();
-    req.dateCreated = formatDate(date);
-    next();
-}
-
-module.exports = dateCreated;
