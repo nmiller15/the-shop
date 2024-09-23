17faf29973706cff621ddb80d16dd0929b9f8dce
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-23 15:48:21 -0400

    add cart model

---
+++ backend/models/Cart.js
@@ -0,0 +1,12 @@
+class Cart {
+  constructor(username) {
+    this.username = username;
+    this.id = null;
+    this.totalCost = 0;
+    this.contents = [];
+  }
+
+  // addToCart(product) {}
+
+  // removeFromCart(product) {}
+}
