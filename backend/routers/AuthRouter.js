af88ffa4a0f1bd46990bc4fd89d91374bad1dd6c
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-27 14:38:27 -0400

    add auth router

---
+++ backend/routers/AuthRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const AuthRouter = express.Router();
+const AuthController = require("../controllers/Authentication");
+
+AuthRouter.post("/login", (req, res, next, body) =>
+  AuthController.login(req, res, next, body)
+);
+AuthRouter.post("/logout", (req, res, next) =>
+  AuthController.logout(req, res, next)
+);
+
+module.exports = AuthRouter;
