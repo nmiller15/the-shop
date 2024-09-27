e45b95dcd6e4b56cbf812ff185617e7d89dd33b6
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-27 14:56:27 -0400

    add address router

---
+++ backend/routers/AddressRouter.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const AddressRouter = express.Router();
+const AddressController = require("../controllers/Address");
+
+AddressRouter.get("/address", (req, res, next, session_id, csrf_token) => {
+  AddressController.getAllAddresses(req, res, next, session_id, csrf_token);
+});
+
+AddressRouter.route("/address/:userId")
+  .get((req, res, next, session_id, csrf_token, userId) => {
+    AddressController.getAddressesByUserId(
+      req,
+      res,
+      next,
+      session_id,
+      csrf_token,
+      userId
+    );
+  })
+  .post((req, res, next, body, userId, session_id, csrf_token) => {
+    AddressController.addAddressByUserId(
+      req,
+      res,
+      next,
+      body,
+      userId,
+      session_id,
+      csrf_token
+    );
+  });
+
+AddressRouter.route("/address/:userId/:id")
+  .put((req, res, next, body, userId, id, session_id, csrf_token) => {
+    AddressController.updateAddressById(
+      req,
+      res,
+      next,
+      body,
+      userId,
+      id,
+      session_id,
+      csrf_token
+    );
+  })
+  .delete((req, res, next, session_id, csrf_token, userId, id) => {
+    AddressController.removeAddressById(
+      req,
+      res,
+      next,
+      session_id,
+      csrf_token,
+      userId,
+      id
+    );
+  });
+
+module.exports = AddressRouter;
