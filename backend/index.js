ca401931996dec77ac20c1544d9652ae6cf33f8c
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-30 10:47:33 -0400

    mount routers in server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const {
+  AuthRouter,
+  UserRouter,
+  AddressRouter,
+  ProductRouter,
+  OrderRouter,
+  CartRouter,
+} = require("./routers");
 
 const app = express();
 const port = 8080;
@@ -7,6 +15,12 @@ const port = 8080;
 app.use(morgan("tiny"));
 
 app.get("/test", (req, res) => res.json("test ok"));
+app.use("/auth", AuthRouter);
+app.use("/user", UserRouter);
+app.use("/address", AddressRouter);
+app.use("/product", ProductRouter);
+app.use("/order", OrderRouter);
+app.use("/cart", CartRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
