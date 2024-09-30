e31890b92622e5f965a881a0a8737690bde4efcb
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-09-30 10:37:57 -0400

    add barrel

---
+++ backend/routers/index.js
@@ -0,0 +1,13 @@
+const UserRouter = require("./UserRouter");
+const AddressRouter = require("./AddressRouter");
+const ProductRouter = require("./ProductRouter");
+const OrderRouter = require("./OrderRouter");
+const CartRouter = require("./CartRouter");
+
+module.exports = {
+  UserRouter,
+  AddressRouter,
+  ProductRouter,
+  OrderRouter,
+  CartRouter,
+};
