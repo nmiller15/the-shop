998702e0dfdc0dc8f61e7fcbf4c88dce30bb752c
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-10-03 14:46:05 -0400

    init supertokens frontend

---
+++ backend/utils/authenticateUser.js
@@ -0,0 +1,7 @@
+const authenticateUser = (req, res, next) => {
+  const sessionId = req.cookie.session_id;
+
+  // query the database to find a session
+
+  // query the database for the user information that matches the session id
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,53 @@ import LoginForm from "./LoginForm";
 import Layout from "./Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import ThirdParty, {
+  Github,
+  Google,
+  Facebook,
+  Apple,
+} from "supertokens-auth-react/recipe/thirdparty";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+
+SuperTokens.init({
+  appInfo: {
+    appName: "the shop",
+    apiDomain: "localhost:8080",
+    websiteDomain: "localhost:3000",
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth",
+  },
+  recipeList: [
+    ThirdParty.init({
+      signInAndUpFeature: {
+        providers: [
+          Github.init(),
+          Google.init(),
+          Facebook.init(),
+          Apple.init(),
+        ],
+      },
+    }),
+    EmailPassword.init(),
+    Session.init(),
+  ],
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<ProductsList />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/products/:id" element={<ProductPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <SuperTokensWrapper>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<ProductsList />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/products/:id" element={<ProductPage />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </SuperTokensWrapper>
   );
 }
 
